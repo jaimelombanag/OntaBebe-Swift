6e54244d721c11567e7d86be1966437f0a9ade50
jaimelombanag <alombana24@hotmail.com>
2018-09-21 13:11:38 -0500

    Listo el programa para subir al AppStore

--- OntaBebe/OntaBebe/ViewController.swift
+++ OntaBebe/OntaBebe/ViewController.swift
@@ -16,10 +16,31 @@ class ViewController: UIViewController {
     
     var webView: WKWebView!
     @IBOutlet weak var webVista: WKWebView!
+    @IBOutlet weak var gifView: UIImageView!
+    @IBOutlet weak var vistaGif: UIView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        webVista.isHidden = true
+        vistaGif.isHidden = false
+        gifView.loadGif(name: "init")
+        
+        
+        
+        let timeout = DispatchTime.now() + .seconds(5)
+        DispatchQueue.main.asyncAfter(deadline: timeout) {
+            self.webVista.isHidden = false
+            self.vistaGif.isHidden = true
+        }
+        
+        
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
